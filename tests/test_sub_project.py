e332ebb0ce52d6adacecda66d83b443c0e82b073
Chris Brown <cbrown1234@hotmail.co.uk>
2025-04-26 22:39:37 +0100

    chore: Refactor tests

---
+++ tests/test_sub_project.py
@@ -0,0 +1,50 @@
+"""Tests for the copier template."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from copier import run_copy
+import pytest
+from pytest_virtualenv import VirtualEnv
+
+from helpers import git_save
+
+TEMPLATE_DIR = Path(__file__).parent.parent.absolute()
+ANSWER_FILE_DEFAULT = '.copier-answers.copier-template.yml'
+
+
+@pytest.fixture
+def sub_project(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    sub_project = tmp_path_factory.mktemp('sub_project')
+    run_copy(
+        str(TEMPLATE_DIR),
+        sub_project,
+        vcs_ref='HEAD',
+        defaults=True,
+        unsafe=True,
+    )
+    git_save(sub_project)
+    return sub_project
+
+
+def test_sub_project_copy_default(
+    sub_project: Path, tmp_path_factory: pytest.TempPathFactory
+) -> None:
+    assert (sub_project / ANSWER_FILE_DEFAULT).exists()
+    sub_sub_project = tmp_path_factory.mktemp('sub_sub')
+    run_copy(
+        str(sub_project),
+        sub_sub_project,
+        vcs_ref='HEAD',
+        defaults=True,
+        unsafe=True,
+    )
+    assert (sub_sub_project / '.copier-answers.your-template-name.yml').exists()
+
+
+def test_sub_project_tests(sub_project: Path, virtualenv: VirtualEnv) -> None:
+    virtualenv.run(
+        'task dev-setup:virtualenv && task test',
+        cd=sub_project,
+    )
