d028b5ed7c98def369b8fe9801bc60d57517634d
Chris Brown <cbrown1234@hotmail.co.uk>
2026-02-14 17:35:25 +0000

    feat: Add plumbum test example

--- tests/sub_project/test_tasks.py
+++ tests/sub_project/test_tasks.py
@@ -1,32 +1,28 @@
-"""Tests for the copier template."""
+"""Tests for the copier template using plumbum."""
 
 from __future__ import annotations
 
-import subprocess
 from pathlib import Path
+
+from plumbum import local
+from plumbum.cmd import pre_commit, task
 
 from tests.helpers import git_save
 
 
 def test_dev_setup(sub_project: Path) -> None:
-    subprocess.run(
-        'pre-commit sample-config > .pre-commit-config.yaml',
-        cwd=sub_project,
-        shell=True,
-        check=True,
-    )
-    git_save(sub_project)
-    subprocess.run('task dev-setup', cwd=sub_project, shell=True, check=True)
+    with local.cwd(sub_project):
+        (pre_commit['sample-config'] > '.pre-commit-config.yaml')()
+        git_save(sub_project)
+        task('dev-setup')
 
 
 def test_docker_mounts(sub_project: Path) -> None:
-    subprocess.run('task example:docker-mount', cwd=sub_project, shell=True, check=True)
+    with local.cwd(sub_project):
+        task('example:docker-mount')
 
 
 def test_sub_project_tests(sub_project: Path) -> None:
-    subprocess.run(
-        'task dev-setup:venv && task test',
-        cwd=sub_project,
-        shell=True,
-        check=True,
-    )
+    with local.cwd(sub_project):
+        task('dev-setup:venv')
+        task('test')
